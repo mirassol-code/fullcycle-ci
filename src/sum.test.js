a9581b21bab06104512eafbce741e06a87dc06b3
Eduardo Martins <mirassol.code@gmail.com>
2021-08-19 16:39:39 -0300

    Added one more function

--- src/sum.test.js
+++ src/sum.test.js
@@ -1,5 +1,10 @@
 const sum = require('./sum');
+const subtract = require('./sum');
 
 test('add 1 + 2 to be equal 3', () =>{
     expect(sum(1, 2)).toBe(3);
+})
+
+test('subtract 3 - 2 to be equal 1', () =>{
+    expect(subtract(3, 2)).toBe(1);
 })
